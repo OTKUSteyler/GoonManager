8c26fe34b71ede3d53f3348b6e2534de69877203
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-11-10 23:01:49 +0100

    Update RGBTextField.kt

--- app/src/main/kotlin/com/aliucord/manager/ui/screens/iconopts/components/RGBTextField.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/screens/iconopts/components/RGBTextField.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import dev.shiggy.manager.R
+import dev.goon.manager.R
 import java.util.Locale
 import kotlin.math.roundToInt
 
