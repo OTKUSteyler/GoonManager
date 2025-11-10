c28d379314d8aefe84c65a724ab04969c1fc159a
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-11-10 21:22:59 +0100

    Update StepGroup.kt

--- app/src/main/kotlin/com/aliucord/manager/patcher/steps/StepGroup.kt
+++ app/src/main/kotlin/com/aliucord/manager/patcher/steps/StepGroup.kt
@@ -3,7 +3,7 @@ package com.aliucord.manager.patcher.steps
 import android.os.Parcelable
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Immutable
-import dev.shiggy.manager.R
+import dev.goon.manager.R
 import kotlinx.parcelize.Parcelize
 
 /**
