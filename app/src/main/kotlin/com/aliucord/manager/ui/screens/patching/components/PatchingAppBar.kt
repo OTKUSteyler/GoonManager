0d7f7308417e7949ef3655ae67c84022167463bc
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-11-10 22:42:00 +0100

    Update PatchingAppBar.kt

--- app/src/main/kotlin/com/aliucord/manager/ui/screens/patching/components/PatchingAppBar.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/screens/patching/components/PatchingAppBar.kt
@@ -4,7 +4,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import dev.shiggy.manager.R
+import dev.goon.manager.R
 
 @Composable
 fun PatchingAppBar(
