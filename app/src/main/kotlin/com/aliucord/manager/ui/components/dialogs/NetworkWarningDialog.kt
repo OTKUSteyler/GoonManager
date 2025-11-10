395a9ca595801b528b75829fe51efc3f0ad7f71d
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-11-10 21:57:42 +0100

    Update NetworkWarningDialog.kt

--- app/src/main/kotlin/com/aliucord/manager/ui/components/dialogs/NetworkWarningDialog.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/components/dialogs/NetworkWarningDialog.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
-import dev.shiggy.manager.R
+import dev.goon.manager.R
 
 @Composable
 fun NetworkWarningDialog(
