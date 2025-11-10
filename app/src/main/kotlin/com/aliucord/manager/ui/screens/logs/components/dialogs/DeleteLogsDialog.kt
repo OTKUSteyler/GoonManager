d5a6647f01f22c256151625a4e15a14ee14b6267
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-11-10 22:44:10 +0100

    Update DeleteLogsDialog.kt

--- app/src/main/kotlin/com/aliucord/manager/ui/screens/logs/components/dialogs/DeleteLogsDialog.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/screens/logs/components/dialogs/DeleteLogsDialog.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import dev.shiggy.manager.R
+import dev.goon.manager.R
 
 @Composable
 fun DeleteLogsDialog(
