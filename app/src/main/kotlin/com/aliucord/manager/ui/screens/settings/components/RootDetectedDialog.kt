c4c20aeeaa9065b319cba85353e4a6d4364bb74f
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-11-10 22:24:06 +0100

    Update RootDetectedDialog.kt

--- app/src/main/kotlin/com/aliucord/manager/ui/screens/settings/components/RootDetectedDialog.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/screens/settings/components/RootDetectedDialog.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import dev.shiggy.manager.R
+import dev.goon.manager.R
 
 @Composable
 fun RootDetectedDialog(
