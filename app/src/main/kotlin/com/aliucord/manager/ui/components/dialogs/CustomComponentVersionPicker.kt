c20c2b88ade51b6ee7b6908a5ec1485cec354912
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-11-10 21:56:11 +0100

    Update CustomComponentVersionPicker.kt

--- app/src/main/kotlin/com/aliucord/manager/ui/components/dialogs/CustomComponentVersionPicker.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/components/dialogs/CustomComponentVersionPicker.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import com.aliucord.manager.network.utils.SemVer
-import dev.shiggy.manager.R
+import dev.goon.manager.R
 
 @Composable
 fun CustomComponentVersionPicker(
