3aae7baef55f5b92553308b3d93174b418c072d9
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-11-10 22:23:21 +0100

    Update ShizukuInfoDialog.kt

--- app/src/main/kotlin/com/aliucord/manager/ui/screens/settings/components/ShizukuInfoDialog.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/screens/settings/components/ShizukuInfoDialog.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import dev.shiggy.manager.R
+import dev.goon.manager.R
 
 @Composable
 fun ShizukuInfoDialog(
